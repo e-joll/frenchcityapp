bd9804e7c3f780ad0afab7cea60243ccc0d76dd3
e-joll <71887317+e-joll@users.noreply.github.com>
2025-03-29 20:20:35 +0100

    refactor: rename variables for better clarity

--- src/main/java/me/example/cityapi/controller/HomeController.java
+++ src/main/java/me/example/cityapi/controller/HomeController.java
@@ -2,15 +2,19 @@ package me.example.cityapi.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HomeController {
     @FXML
-    public Label titleText;
+    public Label homeTitle;
+    
+    @FXML
+    public Button showButton;
 
     public HomeController() { }
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onShowButtonClick() {
         System.out.println("Hello World!");
     }
-} 
+}
